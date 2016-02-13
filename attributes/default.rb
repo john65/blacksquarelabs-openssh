a9fbcaebd191fb4c63e552450e6af1bf7d0befa7
John Coleman <john.coleman@yale.edu>
2016-02-13 16:05:38 -0500

    turn on x11 forwarding

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default['openssh']['server']['permit_root_login'] = 'without-password'
 default['openssh']['server']['password_authentication'] = 'no'
 default['openssh']['server']['use_privilege_separation'] = 'sandbox'
+default['openssh']['server']['x11_forwarding'] = 'yes'
