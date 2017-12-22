d0330bada7989dad88d0ebf4b15f4def569d7959
John Coleman <john.coleman@yale.edu>
2017-12-22 08:31:43 -0500

    privilege separation deprecated. causing noise in logs. removed.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,3 @@
 default['openssh']['server']['permit_root_login'] = 'without-password'
 default['openssh']['server']['password_authentication'] = 'no'
-default['openssh']['server']['use_privilege_separation'] = 'sandbox'
 default['openssh']['server']['x11_forwarding'] = 'yes'
